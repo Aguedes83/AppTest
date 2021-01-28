7cb013b0d5b98a07b5a6462c5b7fa7ec139c1c00
Aguedes83 <guedes.amg@hotmail.com>
2021-01-27 23:04:21 -0200

    versão 1.0.0

---
+++ application/models/aluno1/controllers/PessoaController.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of PessoaController
+ *
+ * @author adm
+ */
+class PessoaController extends Zend_Controller_Action {
+    //put your code here
+    public function init(){
+        /*Inicialize action controller here*/
+    }
+    public function indexAction(){
+        // action body
+    }
+}
+
+?>
---
+++ application/models/aluno1/models/Pessoa.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Pessoa
+ *
+ * @author adm
+ */
+class Aluno1_Model_Pessoa {
+    //put your code here
+}
+
+?>
